3b7240f4995cbd15c865bde1ea4e23b873d8e7ce
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-09-16 12:19:28 +0530

    remove from cart added

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -4,16 +4,28 @@ import { store } from '../context/store';
 const SingleProduct = () => {
   const {productArray,setCart,cart} = useContext(store);
 
-  const setCartTrue = (product) =>{
+  const setToCart = (product) =>{
     product.isAddedToCart = true;
     setCart([...cart,product])
+
+    // console.log(cart)
 
     // FIXME // TODO IF I DO CONSOLE.LOG(cart) HERE THEN I 
     // WILL NOT GET THE UPDATED OBJ IN CART BECAUSE IT RE-RENDER THE COMP.
     // AND THE INITIALLY CART HAS EMPTY VALUE SO IF WHEN USER SECOND TIME CLICK
     // IT WILL SHOW THE UPDATED OBJ IF I TRY IT OUTSIDE THEN IT WILL BEHAVE AS EXPECTED
   };
-  
+
+
+  const removeFromCart = (product) =>{
+    product.isAddedToCart = false;
+    setCart((cart)=>(
+      cart.filter((c)=>(
+        c.id!==product.id
+      ))
+    ))
+  }
+
   return (
     <>
       {
@@ -23,8 +35,8 @@ const SingleProduct = () => {
           <div><h3>{product.name}</h3><h4>{product.price}</h4></div>
           <h1>{product.id}</h1>
           {product.isAddedToCart?
-          <button>Remove from cart</button>
-          :<button onClick={()=>{setCartTrue(product)}}>Add to cart</button>}
+          <button onClick={()=>{removeFromCart(product)}}>Remove from cart</button>
+          :<button onClick={()=>{setToCart(product)}}>Add to cart</button>}
         </div>
       ))
       }
